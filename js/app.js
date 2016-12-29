d3186d32dbf57185ca4496ba36373101a585c096
NikolaTomic <nikola@uidigital.com>
2016-12-29 10:00:45 +0100

    comment

--- js/app.js
+++ js/app.js
@@ -20,28 +20,29 @@ var GameState = {
 		this.scale.pageAlignHorizontally = true;
 		/* start physics engine */
 		this.game.physics.startSystem(Phaser.Physics.ARCADE);
-		this.game.physics.arcade.gravity.y = 0;
 		/* acrivate keyboard */
 		this.cursor = this.game.input.keyboard.createCursorKeys();
 		this.game.world.setBounds(0, 0, 575, 575);
 		this.RUNNIGN_SPEED = 10;
 		this.JUMPING_SPEED = 350;
-		//this.ACCLERATION = 600;
-		//this.DRAG = 400;
-		//this.MAXSPEED = 400;
+		this.ACCLERATION = 600;
+		this.DRAG = 400;
+		this.MAXSPEED = 400;
 	},
 	/* load the game assets before the game starts */
 	preload : function() {
 		/* background */
 		this.load.image('background', 'assets/images/background-game.png');
-		/* sound */
-		this.load.audio('theme', ['assets/audio/MoozE_Predator.mp3']);
 		/* player */
 		this.load.image('player', 'assets/images/su37.png');
 		/* enemy */
 		this.load.image('enemy', 'assets/images/su25.png');
 		/* missile */
 		this.load.image('missile', 'assets/images/missile.png');
+		/* sound */
+		this.load.audio('theme', ['assets/audio/MoozE_Predator.mp3']);
+		this.load.audio('missile_launch', ['assets/audio/Missle_Launch.mp3']);
+		this.load.audio('missile_impact', ['assets/audio/Missile_Impact.mp3']);
 	},
 	/* executed after everything is loaded */
 	create : function() {
@@ -55,18 +56,15 @@ var GameState = {
 		this.player.anchor.setTo(0.5);
 		this.player.scale.setTo(0.25);
 		this.game.physics.arcade.enable(this.player);
-		this.player.body.gravity.y = 1000;
 		this.player.body.collideWorldBounds = true;
-		this.player.body.allowGravity = true;
-		this.player.body.maxVelocity.setTo(400, 400);
-		this.player.body.drag.setTo(400, 400);
+		this.player.body.maxVelocity.setTo(this.MAXSPEED, this.MAXSPEED);
+		this.player.body.drag.setTo(this.DRAG, this.DRAG);
 		/* enemy */
 		//this.enemy = this.add.group();
 		this.enemy = this.game.add.sprite(this.world.centerX, 50, 'enemy');
 		this.enemy.anchor.setTo(0.5, 0.5);
 		this.enemy.scale.setTo(0.25);
 		this.game.physics.arcade.enable(this.enemy);
-		this.enemy.body.allowGravity = true;
 		this.enemy.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
 		/* missile */
 	    this.missile = game.add.weapon(30, 'missile');
@@ -93,6 +91,8 @@ var GameState = {
 		music = game.add.audio('theme');
 		music.volume = 0.5;
     	music.play();
+    	missile_launch = game.add.audio('missile_launch');
+    	missile_impact = game.add.audio('missile_impact');
 	},
 	/* this is executed multiple times per second */
 	update : function() {
@@ -105,27 +105,26 @@ var GameState = {
 		if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
 			this.player.x -= this.RUNNIGN_SPEED;
 			this.player.body.velocity.x = 75;
-			this.player.body.acceleration.x = 600;
+			this.player.body.acceleration.x = this.ACCLERATION;
 		} else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
 			this.player.x += this.RUNNIGN_SPEED;
 			this.player.body.velocity.x = -75;
-			this.player.body.acceleration.x = -600;
+			this.player.body.acceleration.x = -this.ACCLERATION;
 		}
-		/*if ( game.input.keyboard.isDown( Phaser.Keyboard.UP ) ) { //&& this.player.body.touching.down
-		 this.player.y -= 4;
-		 } else if ( game.input.keyboard.isDown( Phaser.Keyboard.DOWN ) ) {
-		 this.player.y += 4;
-		 }*/
+		if ( game.input.keyboard.isDown( Phaser.Keyboard.UP ) ) {
+			
+		}
+		if ( game.input.keyboard.isDown( Phaser.Keyboard.DOWN ) ) {
+			
+		}
 		if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-			//this.player.body.velocity.y = -this.JUMPING_SPEED;
+			missile_launch.play();
 			this.missile.fire();
 		}
 		// Squish and rotate ship for illusion of "banking"
-		bank = this.player.body.velocity.x / 400;
+		bank = this.player.body.velocity.x / this.MAXSPEED;
 		this.player.scale.x = 0.25 - Math.abs(bank) / 2;
 		this.player.angle = bank * 30;
-		
-		//this.enemy.reset(game.rnd.integerInRange(0, game.width), -20);
 	},
 	render : function render() {
 		//game.debug.spriteInfo(this.player, 32, 100);
